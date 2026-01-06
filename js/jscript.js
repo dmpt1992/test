8714d0570697981006e7364b99d164b3696fb71c
dmpt1992 <developwebsitedanielamedeira@gmail.com>
2026-01-06 16:36:48 +0000

    Create jscript.js

---
+++ js/jscript.js
@@ -0,0 +1,24 @@
+const images = [
+    'img/bg1.jpeg',
+    'img/bg2.jpeg',
+    'img/bg3.jpeg',
+    'img/bg4.jpeg',
+    'img/bg5.jpeg'
+];
+
+let current = 0;
+const slider = document.querySelector('.slider');
+
+// inicial
+slider.style.backgroundImage = `url(${images[current]})`;
+
+setInterval(() => {
+    slider.style.opacity = 0;
+
+    setTimeout(() => {
+        current = (current + 1) % images.length;
+        slider.style.backgroundImage = `url(${images[current]})`;
+        slider.style.opacity = 1;
+    }, 600);
+
+}, 5000);
